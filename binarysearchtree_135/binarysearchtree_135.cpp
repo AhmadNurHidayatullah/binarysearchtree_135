d73fd89eaff9d562e1736112b0f9587520263d18
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-07-02 13:03:52 +0700

    menambahkan fungsi void preorder

--- binarysearchtree_135/binarysearchtree_135.cpp
+++ binarysearchtree_135/binarysearchtree_135.cpp
@@ -88,6 +88,16 @@ public:
 
 	void preorder(node* ptr)
 	{
-
+		if (ROOT == NULL)
+		{
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
 	}
 };
